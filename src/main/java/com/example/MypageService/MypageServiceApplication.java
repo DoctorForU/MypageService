b64d6bd2c891a2ca6dda52d758d5a6b228d8f8f8
TirTir <uj0791navercom@skuniv.ac.kr>
2024-06-14 17:47:56 +0900

    feat. init

---
+++ src/main/java/com/example/MypageService/MypageServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.MypageService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class MypageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypageServiceApplication.class, args);
+	}
+
+}
